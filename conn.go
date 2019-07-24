98248a74b3c23454f480a41248dec32c865bbafa
Sam Claus <samterainsights@gmail.com>
2019-07-24 17:56:59 -0400

    Remove unnecessary locking

--- conn.go
+++ conn.go
@@ -3,28 +3,18 @@ package sftp
 import (
 	"encoding"
 	"io"
-	"sync"
 )
 
-// conn implements a bidirectional channel on which client and server
-// connections are multiplexed.
+// conn implements packetSender on top of an io.ReadWriter and needs
+// to be removed, just like packetSender.
 type conn struct {
-	io.Reader
-	io.WriteCloser
-	sync.Mutex // used to serialise writes to sendPacket
-	// sendPacketTest is needed to replicate packet issues in testing
-	sendPacketTest func(w io.Writer, m encoding.BinaryMarshaler) error
+	io.ReadWriter
 }
 
-func (c *conn) sendPacket(m encoding.BinaryMarshaler) error {
-	c.Lock()
-	defer c.Unlock()
-	if c.sendPacketTest != nil {
-		return c.sendPacketTest(c, m)
-	}
-	return writePacket(c, m)
+func (c *conn) sendPacket(pkt encoding.BinaryMarshaler) error {
+	return writePacket(c, pkt)
 }
 
-func (c *conn) sendError(p ider, err error) error {
-	return c.sendPacket(statusFromError(p, err))
+func (c *conn) sendError(pkt ider, err error) error {
+	return c.sendPacket(statusFromError(pkt, err))
 }
